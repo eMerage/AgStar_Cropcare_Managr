a33976adc3751ade849321f420e8cc4a253019f9
eMergeSolutionPVT <emerge_github@friendtho.com>
2019-09-06 16:09:52 +0530

    Initial commit

---
+++ app/src/main/java/project/emarge/cropcare_manager/viewModels/products/ProductsViewModel.kt
@@ -0,0 +1,15 @@
+package project.emarge.cropcare_manager.viewModels.products
+
+import android.app.Application
+
+import androidx.lifecycle.AndroidViewModel
+
+
+open class ProductsViewModel(application: Application) : AndroidViewModel(application) {
+
+
+
+
+
+    
+}
